7ce2184ad4d21728662a8db4f6413b1d7b1818cd
Ken A Collins <Ken@KenACollins.com>
2020-09-27 16:49:12 -0400

    Restarted testing app after some confusion over Jest presence.

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
+// There was a lot of confusion over the fact that the default contents of this file do not look like those in the instruction video.
+// It turned out that both approaches work. Commented out below is the approach with slight changes to the default code.
+/* import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders "Hi There!" without crashing', () => {
+test('renders without crashing', () => {
     const { getByText } = render(<App />);
-    const textElement = getByText(/Hi There!/i);
+    const textElement = getByText(/Hi there!/i);
     expect(textElement).toBeInTheDocument();
+}); */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    expect(div.innerHTML).toContain('Hi there!');
+    ReactDOM.unmountComponentAtNode(div);
 });
+
