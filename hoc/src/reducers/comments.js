ce6171c80cc3000f1bc0ea45671e395b91efff5c
Ken A Collins <Ken@KenACollins.com>
2020-11-25 16:14:21 -0500

    Removed bad state change that triggered stateValidator middleware.

--- hoc/src/reducers/comments.js
+++ hoc/src/reducers/comments.js
@@ -1,9 +1,9 @@
 import { SAVE_COMMENT, FETCH_COMMENTS } from "../actions/types";
 
-export default function(state = [], action) {
+export default function (state = [], action) {
     switch (action.type) {
         case SAVE_COMMENT:
-            return [...state, action.payload, {}];
+            return [...state, action.payload];  // To trigger stateValidator middleware, append ", {}" to the array.
         case FETCH_COMMENTS:
             // debugger; // Insert debugger here temporarily to see what is inside our action.
             // The external API returns JSON array elements with 5 properties. We will just use 'name' as a comment.
